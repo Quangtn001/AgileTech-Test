9be22ba7cc536414fea67b70d1d1ccd00f5d6cf4
quangnb <quangtn001@gmail.com>
2023-08-22 11:53:59 +0700

    fix frofile pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,92 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import { useState } from "react";
 
 const Profile = () => {
+  const [isMobile, setIsMobile] = useState(false);
   return (
     <section>
+      <div className="block lg:hidden mt-10 " onClick={() => setIsMobile(true)}>
+        <svg
+          fill="none"
+          height={50}
+          viewBox="0 0 20 20"
+          width={50}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            d="M3 5C3 4.44772 3.44772 4 4 4H16C16.5523 4 17 4.44772 17 5C17 5.55228 16.5523 6 16 6H4C3.44772 6 3 5.55228 3 5Z"
+            fill="#4A5568"
+            fillRule="evenodd"
+          />
+          <path
+            clipRule="evenodd"
+            d="M3 10C3 9.44772 3.44772 9 4 9H16C16.5523 9 17 9.44772 17 10C17 10.5523 16.5523 11 16 11H4C3.44772 11 3 10.5523 3 10Z"
+            fill="#4A5568"
+            fillRule="evenodd"
+          />
+          <path
+            clipRule="evenodd"
+            d="M3 15C3 14.4477 3.44772 14 4 14H16C16.5523 14 17 14.4477 17 15C17 15.5523 16.5523 16 16 16H4C3.44772 16 3 15.5523 3 15Z"
+            fill="#4A5568"
+            fillRule="evenodd"
+          />
+        </svg>
+      </div>
+      {/* Mobile menu */}
+      <div
+        className={
+          isMobile
+            ? "h-full w-[90%] bg-slate-100 fixed z-50 left-0 top-0 text-white lg:hidden p-5 ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        {/* Render mobile menu items here */}
+        <div className="flex item-start justify-between">
+          <Link
+            to="/"
+            className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start mb-[67px]"
+          >
+            <img src={logo} alt="" />
+            <p className="ml-2 text-xl font-bold text-heading ">
+              DataWarehouse
+            </p>
+          </Link>
+          <div
+            className="absolute right-[500px] top-7"
+            onClick={() => setIsMobile(false)}
+          >
+            <svg
+              height="24px"
+              id="Layer_1"
+              style={{ enableBackground: "new 0 0 512 512" }}
+              version="1.1"
+              viewBox="0 0 512 512"
+              width="24px"
+              xmlSpace="preserve"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+              <path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z" />
+            </svg>
+          </div>
+        </div>
+        <div className="flex flex-col gap-[10px] mt-[30px]">
+          <Link
+            className="text-[20px] leading-8 font-normal text-heading ml-[34px]"
+            to="#"
+          >
+            Posts
+          </Link>
+          <Link
+            className="text-[20px] leading-8 font-normal text-heading ml-[34px]"
+            to="#"
+          >
+            Logout
+          </Link>
+        </div>
+      </div>
       <div className="mx-auto max-w-screen-[1920px]  flex gap-[120px]">
         <div className="h-screen w-[320px] bg-[#D9D9D9] hidden lg:block">
           <Link to="/">
@@ -24,6 +107,7 @@ const Profile = () => {
             </Link>
           </div>
         </div>
+
         <div className="mt-[120px] w-[1600px]">
           <div className="flex items-center flex-col gap-10 lg:flex-row justify-between">
             <Link
